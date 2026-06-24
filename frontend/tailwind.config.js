d41c77eb9e771f82617aeffecbf767a16997ed76
Selma <salmahenni8@gmail.com>
2026-06-24 02:20:01 +0100

    dernieres mofdifications annulées

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    path.join(__dirname, 'app/**/*.{js,ts,jsx,tsx,mdx}'),
+    path.join(__dirname, 'components/**/*.{js,ts,jsx,tsx,mdx}'),
   ],
   theme: {
     extend: {
