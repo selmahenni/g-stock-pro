fad4e0a42c8b9afa03be948ec9d5128a0b85c246
Selma <salmahenni8@gmail.com>
2026-06-25 01:20:20 +0100

    fix: suppression de __dirname dans tailwind et postcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    path.join(__dirname, 'app/**/*.{js,ts,jsx,tsx,mdx}'),
-    path.join(__dirname, 'components/**/*.{js,ts,jsx,tsx,mdx}'),
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
